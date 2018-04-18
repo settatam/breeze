74016801b2405266a2a28531e8058d1292309002
Seth Atam <sethatam@Seths-MacBook-Pro.local>
2018-04-17 22:49:17 -0400

    Update Group and Person models

--- classes/Group.php
+++ classes/Group.php
@@ -2,8 +2,15 @@
 
 /*
 */
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require_once 'ListItem.php';
+
 class Group extends ListItem {
 	protected $db_table = "groups";
-	protected $id;
-	protected $name;
+	protected $id_alias = "group_id";
+	protected $group_name;
+	protected $group_id;
 } 
--- classes/Person.php
+++ classes/Person.php
@@ -2,9 +2,27 @@
 
 /*
 */
-class Person extends Database {
-	protected $id;
-	protected $name;
 
-	
+require_once 'ListItem.php';
+
+class Person extends ListItem {
+
+	protected $db_table = "persons";
+	protected $id_alias = "person_id";
+	protected $first_name;
+	protected $last_name;
+	protected $email_address;
+	protected $group_id;
+	protected $state;
+	protected $person_id;
+
+	public static function getAllActive() {
+		$query = "SELECT * FROM persons p INNER JOIN groups  g ON p.group_id = g.group_id AND p.state = 'active' ORDER BY p.first_name ASC";
+
+		$dbh = Database::Init();
+		$result = $dbh->pdo->query($query);
+		//$result = $dbh->fetchAll();
+		return $result->fetchAll();
+	}
+
 } 
